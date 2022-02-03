219d277493000d2f9a919616c3670614edcf2f20
yubin <yubin@gmail.com>
2022-02-03 14:52:58 +0900

    수정

--- src/main/java/concernboard/ConcernBoardVo.java
+++ src/main/java/concernboard/ConcernBoardVo.java
@@ -32,14 +32,14 @@ public class ConcernBoardVo extends CommonVo {
 	private int rownum;//이전글/다음글에서 사용
 	private String noticeYN;
 	private int new_time;//새로운 글(new) 표시
-	private String isopen;//관리자페이지-자유게시판 등록(공개/비공개-radio name)
+	private String isopen;//관리자페이지-고민게시판 등록(공개/비공개-radio name)
 	private char is_user;//이 값이 'Y'이면 사용자화면에서 공개(useYN = 'Y')	
-	private String useYN;//관리자페이지-자유게시판 등록(공지-공개여부)
+	private String useYN;//관리자페이지-고민게시판 등록(공지-공개여부)
 	private String admin_name;
-	public String getRegdate() {
+	public String getRegdate() {  
 		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
 		return sdf.format(this.regdate);
-	}
+	}  
 	
 	public ConcernBoardVo() {
 		this.orderby = "regdate";
